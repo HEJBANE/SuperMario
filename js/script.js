f6ff1ada901bf32574f29c2156fc16fcf8c24efc
kskalli1 <91310240+kskalli1@users.noreply.github.com>
2021-09-29 09:01:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ var y=170;
 var sign='';
 document.onkeydown  = getKeystart;
 document.onkeyup    = getKeyend;
+
+function getKeystart(keyStroke){
+
+isNetscape=(document.getElementById && !document.all);
+touche = (isNetscape) ? keyStroke.which : event.keyCode;
+if ((touche==37))
+{dx=-5;}
+if ((touche==39))
+{dx=5;}
+if ((touche==38) && (t==-50)){t=-12;}
+}
